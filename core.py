7b48294f49bc0c84e95588cac56c86d901ae661b
soroush <soroush_safarii@yahoo.com>
2020-03-17 12:47:42 +0330

    fix core

--- core.py
+++ core.py
@@ -2,10 +2,17 @@ import requests
 
 
 def parts_size(url) -> list :
-    resquest = requests.head(url)
-    file_size = int(resquest.headers['content-Length'])
-    each_split = int(res.headers['content-Length']) // 8
-    split_size = [ x for x in range(0,int(res.headers['content-Length']),each_split)]
-    split_size[6] = file_size
-
+    """
+    split file into 8 part from header
+    param:
+        - name : url
+            - description : provided url
+    responses:
+        - name : split_size
+            - description : list of possible part size ( in byte )
+    """
+    resquest = requests.head(url) # grab header
+    full_size = int(resquest.headers['content-Length']) # full size 
+    split_size = [ x for x in range(0,full_size,full_size // 8)] # create 
+    split_size[8] = full_size # fix least size to remainder byte from split
     return split_size
