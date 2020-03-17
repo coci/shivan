b24aa5c16956c9b5adba4b7ee55134778a9ebd3a
soroush <soroush_safarii@yahoo.com>
2020-03-17 12:48:05 +0330

    download file with split work

--- main.py
+++ main.py
@@ -4,12 +4,43 @@ import time
 import os
 import sys
 from exception import UrlDoesNotExists
+from core import parts_size
 
 class Shivan(object):
     def __init__(self,url):
         self.url = url
+        self._download()
 
+    def _split(self):
+       return parts_size(self.url)
 
+    def _download(self):
+        parts = self._split()
+        path_to_download = str(os.getcwd()) + "/.temp"
+        for i in range(0,len(parts)-1):
+            start = parts[i] + 1 if i > 0 else parts[i]
+            end = parts[i+1]
+            res = requests.get(self.url,headers={"Range": f"bytes={start}-{end}"})
+            with open(path_to_download+f"/part{i}.jpg", 'wb') as f:
+                f.write(res.content)
+
+        
+        files = []
+        final = open(str(os.getcwd())+"/final.jpg","wb")
+        for r, d, f in os.walk(path_to_download):
+            for file in f:
+                if 'jpg' in file:
+                    files.append(os.path.join(r, file))
+        files = sorted(files)
+
+        for i in files:
+            temp = open(i, 'rb')
+            temp = temp.read()
+            final.write(temp)
+        final.close()
+        for i in files:
+            if not 'final.jpg' in i:
+                os.remove(i)
 if __name__ == "__main__":
     try:
         if len(sys.argv) > 1:
@@ -20,4 +51,3 @@ if __name__ == "__main__":
         print("please enter url ......")
         sys.exit(1)
     action = Shivan(url)
-    print(action)
