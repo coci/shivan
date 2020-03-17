dcf69e99fb47b4007992dad18f6106a62db8082a
soroush <soroush_safarii@yahoo.com>
2020-03-17 18:35:23 +0330

    grab file name

--- operation.py
+++ operation.py
@@ -1,5 +1,5 @@
 import requests
-
+import re
 
 def parts_size(url) -> list :
     """
@@ -16,3 +16,7 @@ def parts_size(url) -> list :
     split_size = [ x for x in range(0,full_size,full_size // 8)] # create 
     split_size[8] = full_size # fix least size to remainder byte from split
     return split_size
+
+def grab_file_name(url):
+    file_name = re.findall(r'(?=\w+\.\w{3,4}$).+',url)
+    return file_name[0]
