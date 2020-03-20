8d895e53e06dadfcc59d407d08e36d8dbb2bb9e6
soroush <soroush_safarii@yahoo.com>
2020-03-20 13:13:15 +0330

    add header format

---
+++ __init__.py
@@ -0,0 +1,7 @@
+__author__ = "Soroush safari"
+__credits__ = ["Soroush safari"]
+__license__ = "GNU 3.0"
+__version__ = "1.0"
+__maintainer__ = "Soroush safari"
+__email__ = "soroush_safarii@yahoo.com"
+__status__ = "Dev"
