bcd3ea993ea76503e2756db08eaf479d038e0130
soroush <soroush_safarii@yahoo.com>
2020-04-25 17:33:03 +0430

    fix main

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Shivan(object):
 		self._validate_url()
 		self._prepare()  # preprocess before download
 		self._information()  # print out information about file
-		self._download()
+		self._download() # download file
 
 	def _validate_url(self):
 		check_url(self.url)
@@ -55,7 +55,6 @@ class Shivan(object):
 
 		if os.path.exists(config_file_path):
 			self._config_file = json.load(open(config_file_path,'r'))
-			print(self._config_file)
 		else:
 			self._config_file = {
 				"part" : "",
